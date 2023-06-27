00ea92ce7bafe7028b449d9971446f87aca6e01d
Nata Entali <nata.entali@protonmail.com>
2023-06-27 14:09:00 +0800

    routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Auth from '../../features/Auth'
+import Login from '../../features/Auth/Login'
+import SignUp from '../../features/Auth/SignUp'
 import ChatGpt from '../../features/ChatGPT'
 import './App.css'
 
 const App:React.FC = () => {
+  const [user, setUser] = useState<any>(null)
+
   return (
-    <ChatGpt />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Auth user={user} />}/>
+          <Route path="/login" element={<Login user={user} setUser={setUser} />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/chat" element={<ChatGpt />}/>
+        </Routes>
+      </Router>
   )
 }
 
