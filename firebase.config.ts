28428cabb3d77dfe534656d53109e881746a291f
Nata Entali <nata.entali@protonmail.com>
2023-06-27 14:08:32 +0800

    Firebase Auth; @tanstack/react-query

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +19,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
 const analytics = getAnalytics(app)
 
-export { app, analytics }
+const auth = getAuth()
+auth.languageCode = 'en'
+
+const provider = new GoogleAuthProvider()
+
+const signInWithGoogle = () => signInWithRedirect(auth, provider)
+
+export { app, analytics, auth, signInWithGoogle }
