ca6d899e3d9939f33210d627a965fad4f51b80b5
Nata Entali <nata.entali@protonmail.com>
2023-06-28 13:24:58 +0800

    firebase config updated

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,13 +19,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-const analytics = getAnalytics(app)
-
 const auth = getAuth()
 auth.languageCode = 'en'
 
 const provider = new GoogleAuthProvider()
 
-const signInWithGoogle = () => signInWithRedirect(auth, provider)
-
-export { app, analytics, auth, signInWithGoogle }
+export { app, auth, provider }
